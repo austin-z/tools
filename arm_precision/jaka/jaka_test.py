8bc2bb622db4c049a1db2ac3cbec9e9242b96704
austin-z <153891661+austin-z@users.noreply.github.com>
2024-01-30 09:39:04 +0800

    joint

--- arm_precision/jaka/jaka_test.py
+++ arm_precision/jaka/jaka_test.py
@@ -31,10 +31,7 @@ def save_result(name, columns, result):
         writer.writerow(columns)
         writer.writerows(result)
 
-def test1():
-    robot = jkrc.RC("192.168.0.204")
-    print("login: ", robot.login())
-
+def test1(robot):
     result_tcp = []
     result_joint = []
 
@@ -47,17 +44,10 @@ def test1():
 
         time.sleep(0.5)
 
-    print("logout: ", robot.logout())
-
     save_result("test1_tcp", ['x', 'y', 'z', 'rx', 'ry', 'rz'], result_tcp)
     save_result("test1_joint", ['j1', 'j2', 'j3', 'j4', 'j5', 'j6'], result_joint)
 
-def test2():
-    robot = jkrc.RC("192.168.0.204")
-    print("login:", robot.login())
-    #print("power_on: ", robot.power_on())
-    #print("enable_robot: ", robot.enable_robot())
-
+def test2(robot):
     p0 = get_tcp_position(robot)
     p1 = [item + 10 if i < 3 else item + 0.0175 for i, item in enumerate(p0)]
     print(p0, p1)
@@ -82,19 +72,52 @@ def test2():
             print(i, "diff:", diff)
             result.append(diff)
 
-    #print("disable_robot: ", robot.disable_robot())
-    #print("power_off: ", robot.power_off())
-
-    p = get_tcp_position(robot)
-    diff = [abs(a - b) for a, b in zip(p, p0)]
-    print("final diff:", diff)
-
-    print("logout: ", robot.logout())
-
     save_result("test2_tcp", ['x', 'y', 'z', 'rx', 'ry', 'rz'], result)
+
+def test3():
+    p0 = get_joint_position(robot)
+    p1 = [item + 0.0175 for item in p0]
+    print(p0, p1)
+
+    result = []
+
+    for i in range(0, 50):
+        if i % 2 == 0:
+            robot.joint_move(p1, ABS, True, 5)
+            time.sleep(1)
+
+            p = get_joint_position(robot)
+            diff = [abs(a - b) for a, b in zip(p, p1)]
+            print(i, "diff:", diff)
+            result.append(diff)
+        else:
+            robot.joint_move(p0, ABS, True, 5)
+            time.sleep(1)
+
+            p = get_joint_position(robot)
+            diff = [abs(a - b) for a, b in zip(p0, p)]
+            print(i, "diff:", diff)
+            result.append(diff)
+
+    save_result("test3_joint", ['j1', 'j2', 'j3', 'j4', 'j5', 'j6'], result)
 
 if __name__ == '__main__':
     __common.init_env()
     import jkrc
 
-    test2()
+try:
+    robot = jkrc.RC("192.168.0.204")
+    print("login:", robot.login())
+    #print("power_on: ", robot.power_on())
+    #print("enable_robot: ", robot.enable_robot())
+
+    test3()
+
+except Exception as e:
+  print("An error occurred:", e)
+
+finally:
+    #print("disable_robot: ", robot.disable_robot())
+    #print("power_off: ", robot.power_off())
+    print("logout: ", robot.logout())
+
